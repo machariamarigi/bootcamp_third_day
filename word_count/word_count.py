7008af054463496985afb5300a08fca2273b7d03
machariamarigi <machariamarigi@gmail.com>
2017-06-14 12:02:48 +0300

    Add function words() and test it

--- word_count/word_count.py
+++ word_count/word_count.py
@@ -1,2 +1,26 @@
 def words(statement):
-        pass
+    """
+        Function that takes in a statement and returns a dictionary with words
+        use in in the statement as keys and the number of times the words has
+        been used as values.
+    """
+
+    words_dict = {}
+    if isinstance(statement, str):
+        words = statement.split()
+    else:
+        raise TypeError('Function only takes in strings')
+
+    for word in words:
+        if word not in words_dict:
+            if word.isdigit():
+                words_dict[int(word)] = 1
+            else:
+                words_dict[word] = 1
+        else:
+            if word.isdigit():
+                words_dict[int(word)] += 1
+            else:
+                words_dict[word] += 1
+
+    return words_dict
