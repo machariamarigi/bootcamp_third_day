51cab1a8e8942c4ea8a67a027f6ea428ac0030e5
machariamarigi <machariamarigi@gmail.com>
2017-06-15 16:48:03 +0300

    Add test to enure we are getting a response

--- HTTP_Web/http_web_client.py
+++ HTTP_Web/http_web_client.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen, Request, URLError
+import requests
 import json
 
 
@@ -7,27 +7,13 @@ def http_web_client(url):
         Client that consumes a public web API
     """
 
-    request = Request(url)
-
-    try:
-        response = urlopen(request)
-        result = json.loads(response.read().decode('utf-8'))
-
-    except URLError as error:
-        return {"Error": str(error)}
-
-    else:
-        if result['city']['country']:
-            return result['city']['country']
-        else:
-            return result
 
 
-def request_url():
-    """
-        Function to prompt user enter an url of a public ApI
-    """
-    input_url = input('Please enter a public API url: ')
-    print(http_web_client(input_url))
+# def request_url():
+#     """
+#         Function to prompt user enter an url of a public ApI
+#     """
+#     input_url = input('Please enter a public API url: ')
+#     print(http_web_client(input_url))
 
-request_url()
+# request_url()
